f0e62692551b517e38864ef8da2629bf3dce9195
Joao Pedro Marcon <jpmarcon.10@gmail.com>
2025-07-18 16:57:15 -0300

    Service layer, component registration

--- src/main/java/com/marcon/course/repositories/UserRepository.java
+++ src/main/java/com/marcon/course/repositories/UserRepository.java
@@ -2,8 +2,9 @@ package com.marcon.course.repositories;
 
 import com.marcon.course.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
 
 }
