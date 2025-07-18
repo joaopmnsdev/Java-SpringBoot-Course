95add4c7e0829d9f25938c5836a292c939d8a809
Joao Pedro Marcon <jpmarcon.10@gmail.com>
2025-07-18 17:39:46 -0300

    Order, instant, ISO 8601 - Part II

---
+++ src/main/java/com/marcon/course/repositories/OrderRepository.java
@@ -0,0 +1,10 @@
+package com.marcon.course.repositories;
+
+import com.marcon.course.entities.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}
---
+++ src/main/java/com/marcon/course/services/OrderService.java
@@ -0,0 +1,26 @@
+package com.marcon.course.services;
+
+import com.marcon.course.entities.Order;
+import com.marcon.course.repositories.OrderRepository;
+import com.marcon.course.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class OrderService {
+
+    @Autowired
+    private OrderRepository repository;
+
+    public List<Order> findAll(){
+        return repository.findAll();
+    }
+
+    public Order findById(Long id){
+        Optional<Order> obj = repository.findById(id);
+        return obj.get();
+    }
+}
