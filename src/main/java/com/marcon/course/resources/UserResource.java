ea85b4cd2a8a33c9240411adadb7d0f40c590a44
João Pedro Marcon <joao.marcon@joi.contaazul.local>
2025-07-18 16:57:15 -0300

    Service layer, component registration

--- src/main/java/com/marcon/course/resources/UserResource.java
+++ src/main/java/com/marcon/course/resources/UserResource.java
@@ -1,18 +1,34 @@
 package com.marcon.course.resources;
 
 import com.marcon.course.entities.User;
+import com.marcon.course.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping
-    public ResponseEntity<User> findAll(){
-        User firstUser = new User(1L, "João", "Joao@gmail.com", "4799999999", "12345678");
-        return ResponseEntity.ok().body(firstUser);
+    public ResponseEntity<List<User>> findAll(){
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
     }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        User u = service.findById(id);
+        return ResponseEntity.ok().body(u);
+    }
+
+
 }
