0ce27ee531bf9d9844ff9e0bea89d669574db825
Joao Pedro Marcon <jpmarcon.10@gmail.com>
2025-07-25 17:31:55 -0300

    Product entity

--- src/main/java/com/marcon/course/config/TestConfig.java
+++ src/main/java/com/marcon/course/config/TestConfig.java
@@ -2,10 +2,12 @@ package com.marcon.course.config;
 
 import com.marcon.course.entities.Category;
 import com.marcon.course.entities.Order;
+import com.marcon.course.entities.Product;
 import com.marcon.course.entities.User;
 import com.marcon.course.enums.OrderStatus;
 import com.marcon.course.repositories.CategoryRepository;
 import com.marcon.course.repositories.OrderRepository;
+import com.marcon.course.repositories.ProductRepository;
 import com.marcon.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -28,6 +30,9 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private CategoryRepository categoryRepository;
 
+    @Autowired
+    private ProductRepository productRepository;
+
 
     @Override
     public void run(String... args) throws Exception {
@@ -43,6 +48,14 @@ public class TestConfig implements CommandLineRunner {
         Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.DELIVERED, u2);
         Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.SHIPPED, u1);
 
+        Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+        Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+        Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+        Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+        Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
+
+
+        productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
         categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
