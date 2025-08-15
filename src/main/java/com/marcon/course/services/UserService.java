34c01173a6863d6e08b26d84d3c28c34eb104710
João Pedro Marcon <jpmarcon.10@gmail.com>
2025-08-15 16:21:54 -0300

    Entity Not Found Exception

--- src/main/java/com/marcon/course/services/UserService.java
+++ src/main/java/com/marcon/course/services/UserService.java
@@ -4,6 +4,7 @@ import com.marcon.course.entities.User;
 import com.marcon.course.repositories.UserRepository;
 import com.marcon.course.services.execptions.DatabaseException;
 import com.marcon.course.services.execptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -44,9 +45,13 @@ public class UserService {
     }
 
     public User update(Long id, User obj) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
